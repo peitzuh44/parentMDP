3719353366bd7073aa2942f0a805e89e549fb158
peitzuh44 <peitzuh44@berkeley.edu>
2024-02-08 13:58:16 -0800

    Initial Commit

---
+++ parentMDP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  parentMDP
+//
+//  Created by Pei-Tzu Huang on 2024/2/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ parentMDP/parentMDPApp.swift
@@ -0,0 +1,17 @@
+//
+//  parentMDPApp.swift
+//  parentMDP
+//
+//  Created by Pei-Tzu Huang on 2024/2/8.
+//
+
+import SwiftUI
+
+@main
+struct parentMDPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
